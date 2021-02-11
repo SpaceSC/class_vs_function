6a3bbd5407e66dbe56fc6e6ca2d461c0586ad88b
SpaceSC <ceciliasandor.22@gmail.com>
2021-02-11 11:25:53 +0100

    Add demo div to html and use template literal in js

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 const pageLoad = () => {
   
+  class Car {
+  constructor(name, year) {
+    this.name = name;
+    this.year = year;
+  }
+  age() {
+    let date = new Date();
+    return date.getFullYear() - this.year;
+  }
+}
+
+let myCar = new Car("Ford", 2014);
+document.getElementById("demo").innerHTML =
+`My car is  ${myCar.age()}  years old.`;
 
 }
 window.addEventListener("load", pageLoad);
