3b9843e58a1a34962acd13de537eb38150a3adae
SpaceSC <ceciliasandor.22@gmail.com>
2021-02-11 13:32:38 +0100

    Use object constructor with good syntax

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const pageLoad = () => {
  
+  /*
   class Car {
-
+    
    // construktor kapja meg a paramétereket, majd this.year-rel hivatkozunk pl. rá 
     constructor(name, year) {
       this.name = name;
@@ -17,13 +18,45 @@ const pageLoad = () => {
     ifOld() {
       return this.age() < 10 ? "a new" : "an old";
     }
+  */
+ 
+ // constructor megoldás
+
+ function Car(name, year) {
+  this.name = name;
+  this.year = year;
+  this.age = function age() {
+      let date = new Date();
+      return date.getFullYear() - this.year;
+    };
+  this.old = function ifOld() {
+      return this.age() < 10 ? "a new" : "an old";
+    };
   }
+
+
 // itt példányosítjuk, ekkor fut le:
 let myCar = new Car("Ford", 2014);
 
 document.getElementById("root").innerHTML =
-`My car is a ${myCar.name}, and is ${myCar.age()} years old. This is ${myCar.ifOld()} car.`;
+`My car is a ${myCar.name}, and is ${myCar.age()} years old. This is ${myCar.old()} car.`;
 
+//// Object constructor / prototype / object method ////
+/* constructor
+
+function Car(name, year) {
+  this.name = name;
+  this.year = year;
+  this.age = age() {
+      let date = new Date();
+      return date.getFullYear() - this.year;
+    };
+  this.old = ifOld() {
+      return this.age() < 10 ? "a new" : "an old";
+    };
+}
+
+*/
 
 /*
 functionná alakítás:
@@ -48,7 +81,7 @@ functionná alakítás:
 */
 
 
-// function
+// function esetén
  /* document.getElementById("demo").innerHTML = `My car is a ${car("Ford", 2014).name}, and is ${car("Ford", 2014).age()}  years old. This is ${ifOld()}`;
  */
 
